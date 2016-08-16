5141094ca9247c178e3533be985bcbeac5e0b995
Roman Abendroth <rabend@nb-bn-055.lan.tarent.de>
2016-08-16 12:31:36 +0200

    removed gravatar from leaderboard

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -5,9 +5,7 @@ $(document).ready(function(){
   $.getJSON(leaderboardApiUrl+'/api/leaderboard', function(data){
     var scores = data.data;
     $.each(scores, function(i, item){
-      var hash = MD5(item.mail);
       var $row = '<tr class="leaderBodyRow">'+
-        '<td class="leaderBodyCell">'+'<img src="http://www.gravatar.com/avatar/'+hash+'"/>'+
         '<td class="leaderBodyCell">'+item.name+'</td>'+
         '<td class="leaderBodyCell">'+item.games+'</td>'+
         '<td class="leaderBodyCell">'+item.score+'</td>'+
