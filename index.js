c18b9779265abbb2138e952d3ad5df963cc6c8e0
Lukas Degener <l.degener@tarent.de>
2017-03-18 17:58:10 +0100

    added api entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  AsciiArt = require("./lib/ascii-art"),
+  Bbox = require("./lib/bbox"),
+  Board = require("./lib/board"),
+  MatchMaker = require("./lib/matchmaker"),
+  Pattern = require("./lib/pattern"),
+  Placement = require("./lib/placement")
+};
